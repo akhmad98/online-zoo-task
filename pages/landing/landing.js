e4dae3d5e8b03752b6ae30702acc873c9952e4e5
Akhmad Badalov <akhmadbadalov98@gmail.com>
2026-03-01 13:54:07 +0500

    feat: functions

--- pages/landing/landing.js
+++ pages/landing/landing.js
@@ -13,3 +13,33 @@
         menu.classList.remove('header_nav-active');
     })
 }());
+
+
+const meetGridLayout = document.querySelector('.meet-animal-grid');
+const feedGridLayout = document.querySelector('feed-grid');
+const nextFeedbtn = document.querySelector('.next');
+const prevFeedbtn = document.querySelector('.prev');
+const nextMeetbtn = document.querySelector('.next-btn');
+const prevMeetBtn = document.querySelector('.prev-btn');
+
+nextMeetbtn.addEventListener('click', () => {
+    const cardWidth = document.querySelector('.meet-animal-card').offsetWidth;
+    meetGridLayout.scrollLeft += (cardWidth + 30);
+})
+
+prevMeetBtn.addEventListener('click', () => {
+    const cardWidth = document.querySelector('.meet-animal-card').offsetWidth;
+    meetGridLayout.scrollLeft -= (cardWidth + 30);
+})
+
+nextFeedbtn.addEventListener('click', () => {
+    console.log('b');
+    const cardWidth = document.querySelector('.feed-card');
+    feedGridLayout.scrollLeft += (cardWidth + 30);
+})
+
+prevFeedbtn.addEventListener('click', () => {
+    console.log('bs');
+    const cardWidth = document.querySelector('.feed-card');
+    feedGridLayout.scrollLeft -= (cardWidth + 30);
+})
